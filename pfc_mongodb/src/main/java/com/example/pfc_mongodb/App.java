21b9c6559d37ab86b4329a304866b7c3d306cd9d
sanesloEduca <sandra.estebanlopez@educa.madrid.org>
2024-05-08 00:27:16 +0200

    creation

---
+++ pfc_mongodb/src/main/java/com/example/pfc_mongodb/App.java
@@ -0,0 +1,13 @@
+package com.example.pfc_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfcMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfcMongodbApplication.class, args);
+	}
+
+}
---
+++ pfc_mongodb/src/test/java/com/example/pfc_mongodb/PfcMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfc_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfcMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
