a48dce74745bfb5549dc7d2b999c3053bb7684c5
srt.esteban <sandra.estebanlopez@educa.madrid.org>
2024-05-28 08:40:44 +0200

    create class users

---
+++ pfc_letras/src/main/java/com/letras/pfc_letras/models/UsersModels/UserModel.java
@@ -0,0 +1,32 @@
+package com.letras.pfc_letras.models.UsersModels;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Document(collection = "user")
+public class UserModel{
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String username;
+
+    private String password;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private Set<UserRoles> roles;
+}
---
+++ pfc_letras/src/main/java/com/letras/pfc_letras/models/UsersModels/UserRoles.java
@@ -0,0 +1,5 @@
+package com.letras.pfc_letras.models.UsersModels;
+
+public enum UserRoles {
+    USER,ADMIN
+}
