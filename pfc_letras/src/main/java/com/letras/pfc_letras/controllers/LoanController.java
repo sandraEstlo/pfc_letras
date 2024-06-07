57969c55a53db758d0b6e9985c483c6513126c70
srt.esteban <sandra.estebanlopez@educa.madrid.org>
2024-06-07 13:18:18 +0200

    remove comments

--- pfc_letras/src/main/java/com/letras/pfc_letras/controllers/LoanController.java
+++ pfc_letras/src/main/java/com/letras/pfc_letras/controllers/LoanController.java
@@ -4,20 +4,36 @@ import com.letras.pfc_letras.dtos.loan.CreateLoanDto;
 import com.letras.pfc_letras.dtos.loan.CreateLoanRequestDto;
 import com.letras.pfc_letras.errors.exceptions.loans.ErrorToCreateLoan;
 import com.letras.pfc_letras.facades.Facade;
+import com.letras.pfc_letras.models.UsersModels.UserModel;
+import com.letras.pfc_letras.models.UsersModels.UserRoles;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/letras")
+@RequestMapping("/letras")
 public class LoanController {
 
     @Resource
     private Facade facade;
 
+    @GetMapping("/loan/user")
+    public String viewLoans(Model model, HttpSession session) {
+
+        UserModel userModel = (UserModel) session.getAttribute("usersession");
+        if (userModel != null && userModel.getRoles().contains(UserRoles.USER)) {
+            model.addAttribute("user", facade.getUserDto(userModel).get());
+            return "loans-user";
+        }
+        return "index";
+    }
 
     @PutMapping("/new")
     public ResponseEntity<CreateLoanDto> addNewBookingLoan(@RequestBody CreateLoanRequestDto createLoanRequestDto) {
